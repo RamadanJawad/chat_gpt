479c73fa0d47715463085c81cf63dbe22551e0d4
RamadanJawad <ramadanabojood@gmail.com>
2023-03-18 20:56:52 +0300

    secret key

--- lib/core/api/api_url.dart
+++ lib/core/api/api_url.dart
@@ -1,4 +1,4 @@
 class ApiUrl {
   static const String BASE_URL = "https://api.openai.com/v1";
-  static const String API_KEY = "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";
+  static const String API_KEY = "sk-********************************************";
 }
